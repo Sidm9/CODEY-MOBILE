447492a49a71f3997572ec96ed3bdb15f89f6d9b
SidM9 <43111742+Sidm9@users.noreply.github.com>
2020-03-31 22:06:52 +0530

    All Language Cards Added

--- Frontend/Components/CategoryScreenComponents/InnerFramework.js
+++ Frontend/Components/CategoryScreenComponents/InnerFramework.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import CategoryCard from './CategoryCard';
 import SearchBar from '../SearchBar';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
   categoryCardGridContainer: {
@@ -32,48 +33,89 @@ export default function LowLevel() {
       <View style={styles.MainContainer}>
         <View style={styles.categoryCardMainContainer}>
           <SearchBar title="Search Languages.." />
-          {/*For Each Rows */}
-          <View style={styles.categoryCardGridContainer}>
-            <CategoryCard
-              languageName="React"
-              languageIcon={require('../../assets/images/jsx.png')}
-            />
-            <CategoryCard
-              languageName="Vue"
-              languageIcon={require('../../assets/images/Vue.png')}
-            />
-          </View>
-
-          <View style={styles.categoryCardGridContainer}>
-            <CategoryCard
-              languageName="React Native"
-              languageIcon={require('../../assets/images/react-native.png')}
-            />
-            <CategoryCard
-              languageName="Flask"
-              languageIcon={require('../../assets/images/flask.png')}
-            />
-          </View>
-          <View style={styles.categoryCardGridContainer}>
-            <CategoryCard
-              languageName="Flutter"
-              languageIcon={require('../../assets/images/flutter.png')}
-            />
-            <CategoryCard
-              languageName="Flask"
-              languageIcon={require('../../assets/images/flask.png')}
-            />
-          </View>
-          <View style={styles.categoryCardGridContainer}>
-          <CategoryCard
-            languageName="Next JS"
-            languageIcon={require('../../assets/images/next.png')}
-          />
-          <CategoryCard
-            languageName="Nuxt JS"
-            languageIcon={require('../../assets/images/nuxt.png')}
-          />
-        </View>
+          <ScrollView>
+            {/*For Each Rows */}
+            <View style={styles.categoryCardGridContainer}>
+              <CategoryCard
+                languageName="React"
+                languageIcon={require('../../assets/images/jsx.png')}
+              />
+              <CategoryCard
+                languageName="Vue"
+                languageIcon={require('../../assets/images/Vue.png')}
+              />
+            </View>
+            <View style={styles.categoryCardGridContainer}>
+              <CategoryCard
+                languageName="React Native"
+                languageIcon={require('../../assets/images/react-native.png')}
+              />
+              <CategoryCard
+                languageName="Flask"
+                languageIcon={require('../../assets/images/flask.png')}
+              />
+            </View>
+            <View style={styles.categoryCardGridContainer}>
+              <CategoryCard
+                languageName="Flutter"
+                languageIcon={require('../../assets/images/flutter.png')}
+              />
+              <CategoryCard
+                languageName="Angular JS"
+                languageIcon={require('../../assets/images/angular.png')}
+              />
+            </View>
+            <View style={styles.categoryCardGridContainer}>
+              <CategoryCard
+                languageName="Next JS"
+                languageIcon={require('../../assets/images/next.png')}
+              />
+              <CategoryCard
+                languageName="Nuxt JS"
+                languageIcon={require('../../assets/images/nuxt.png')}
+              />
+            </View>
+            <View style={styles.categoryCardGridContainer}>
+              <CategoryCard
+                languageName="Android Studio"
+                languageIcon={require('../../assets/images/android.png')}
+              />
+              <CategoryCard
+                languageName="X CODE"
+                languageIcon={require('../../assets/images/xcode.png')}
+              />
+            </View>
+            <View style={styles.categoryCardGridContainer}>
+              <CategoryCard
+                languageName="Electron"
+                languageIcon={require('../../assets/images/electron.png')}
+              />
+              <CategoryCard
+                languageName="Peewee"
+                languageIcon={require('../../assets/images/peewee.png')}
+              />
+            </View>
+            <View style={styles.categoryCardGridContainer}>
+              <CategoryCard
+                languageName="SQL Alchemy"
+                languageIcon={require('../../assets/images/SQLA.jpg')}
+              />
+              <CategoryCard
+                languageName="Marshmallow"
+                languageIcon={require('../../assets/images/marshmallow.png')}
+              />
+            </View>
+            <View style={styles.categoryCardGridContainer}>
+              <CategoryCard
+                languageName="Django"
+                languageIcon={require('../../assets/images/django.png')}
+              />
+              <CategoryCard
+                languageName="QT Creator "
+                languageIcon={require('../../assets/images/qtcreator.png')}
+              />
+            </View>
+          </ScrollView>
         </View>
       </View>
     </>
