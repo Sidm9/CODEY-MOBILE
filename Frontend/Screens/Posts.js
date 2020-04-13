d46c28232dadefa30dd221080c6db735197663a9
mechaadi <as808780@gmail.com>
2020-04-14 02:00:28 +0530

    redux init

--- Frontend/Screens/Posts.js
+++ Frontend/Screens/Posts.js
@@ -1,9 +1,15 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, RefreshControl, FlatList, Button } from 'react-native';
 import Card from '../Components/Card';
 import Heading from '../Components/Heading';
 import {array} from '../Database/PostData'; 
+import {fetchPosts} from '../redux/actions/postAction';
+
+import { useSelector, useDispatch } from 'react-redux'
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -24,14 +30,28 @@ function wait(timeout) {
 }
 
 export default function Posts({ navigation }, props) {
+  
+
+  const posts = useSelector(state => state.posts)
+
+  const dispatch = useDispatch()
+  
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, posts.items ? [posts.items.length] : [posts.items])
+
+ 
 
   const [refreshing, setRefreshing] = React.useState(false);
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
-
+    dispatch(fetchPosts())
     wait(2000).then(() => setRefreshing(false));
   });
+  
+
 
 let ar = array;
   return (
@@ -40,11 +60,11 @@ let ar = array;
       <View style={colorScheme != 'dark' ? Darkstyles.container : styles.container} >
         <Heading title="Feed" click={() => navigation.navigate('Profile')} />
         <FlatList
-          data={ar}
-          initialNumToRender={5}
+          initialNumToRender={8}
+          data={posts.items}
           renderItem={({ item }) => (
             <Card
-              imagesss={item.image}
+              imagesss={item.thumbnailUrl}
               heading={item.title}
               auther={item.person_name}
               time={item.time}
